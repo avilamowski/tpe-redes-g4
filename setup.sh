cd2faa8fe4345a5edd30796d1f33ab24d69fe7ad
j0liu <joliu@itba.edu.ar>
2025-05-21 19:18:02 -0300

    fixed chart paths in setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Warning: this is only for local development
+
+#minikube addons enable ingress
+
 eval $(minikube docker-env)
 docker build -t chat-frontend:latest client
 docker build -t message-service:latest server
 
 # helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install postgres bitnami/postgresql --set auth.username=redes --set auth.password=kubernetes --set auth.database=chatdb
-helm install message-service ./message-service-chart 
-helm install chat-frontend ./chat-frontend-chart
-helm install ingress ./ingress-chart
+helm install message-service ./charts/message-service-chart 
+helm install chat-frontend ./charts/chat-frontend-chart
+helm install ingress ./charts/ingress-chart
