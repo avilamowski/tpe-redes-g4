66d8e1c410cf3747dd8c1d06cd82d7a1b424e083
j0liu <joliu@itba.edu.ar>
2025-05-22 19:21:20 -0300

    super basic architecture with terraform

---
+++ terraform/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  ami           = "ami-080e1f13689e07408" # Ubuntu 22.04 LTS (HVM), SDD Volume Type
+  instance_type = "t2.medium"
+
+  #change the location of the key file
+  key_file_name = "~/.ssh/id_ed25519_b.pub"
+  region        = "us-east-1"
+}
