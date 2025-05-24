ef461a34ac3ebde8c1f7e8b7039f5093778e9496
davidwis <davidwis000@gmail.com>
2025-05-24 19:03:59 -0300

    Autoscaling groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,21 +47,72 @@ data "template_file" "worker_user_data" {
   }
 }
 
-resource "aws_instance" "node" {
-  count                       = 2
-  ami                         = local.ami
-  instance_type               = local.instance_type
-  key_name                    = aws_key_pair.node-key.key_name
-  vpc_security_group_ids      = [aws_security_group.node_sg.id]
-  user_data                   = data.template_file.worker_user_data.rendered
-  subnet_id                   = module.vpc.public_subnets[count.index + 1]
-  associate_public_ip_address = true
 
-  depends_on = [aws_instance.master]
+resource "aws_launch_template" "worker_lt" {
+  name_prefix   = "worker-lt-"
+  image_id      = local.ami
+  instance_type = local.instance_type
+  key_name      = aws_key_pair.node-key.key_name
 
-  tags = {
-    Name = "worker-node-${count.index + 1}"
+  vpc_security_group_ids = [aws_security_group.node_sg.id]
+
+  user_data = base64encode(data.template_file.worker_user_data.rendered)
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "worker-node-autoscaled"
+    }
   }
+}
+
+resource "aws_autoscaling_group" "worker_asg" {
+  name_prefix         = "worker-asg-"
+  desired_capacity    = 2
+  max_size            = 5
+  min_size            = 2
+  vpc_zone_identifier = module.vpc.public_subnets
+
+  launch_template {
+    id      = aws_launch_template.worker_lt.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "worker-node-autoscaled"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_policy" "scale_out" {
+  name                   = "scale-out"
+  autoscaling_group_name = aws_autoscaling_group.worker_asg.name
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 60
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu" {
+  alarm_name          = "high-cpu-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 60
+  alarm_description   = "This metric monitors high CPU usage"
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.worker_asg.name
+  }
+
+  alarm_actions = [aws_autoscaling_policy.scale_out.arn]
 }
 
 resource "aws_security_group" "node_sg" {
@@ -84,9 +135,9 @@ resource "aws_security_group" "node_sg" {
   }
 }
 
-output "ssh_commands_to_worker_nodes" {
-  value       = [for public_dns in aws_instance.node.*.public_dns : "ssh ubuntu@${public_dns}"]
-  description = "SSH commands to connect to Kubernetes worker nodes"
+output "worker_nodes_in_asg" {
+  value       = aws_autoscaling_group.worker_asg.name
+  description = "Auto Scaling Group name for the worker nodes"
 }
 
 output "ssh_commands_to_master_node" {
