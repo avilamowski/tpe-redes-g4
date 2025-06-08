1bb2aa102b45aafab08413f8bb252c39fab84593
davidwis <davidwis000@gmail.com>
2025-06-08 16:59:05 -0300

    Fast redeployment of pods when node is unavailable

--- terraform/user_data/master.sh
+++ terraform/user_data/master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 K3S_TOKEN="${token}"
-curl -sfL https://get.k3s.io | K3S_TOKEN="$K3S_TOKEN" sh -
+curl -sfL https://get.k3s.io | K3S_TOKEN="$K3S_TOKEN" INSTALL_K3S_EXEC="--kube-apiserver-arg default-not-ready-toleration-seconds=10 --kube-apiserver-arg default-unreachable-toleration-seconds=10 --kube-controller-arg node-monitor-period=10s --kube-controller-arg node-monitor-grace-period=10s --kubelet-arg node-status-update-frequency=5s" sh -
 
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 git clone https://github.com/avilamowski/tpe-redes-g4.git
--- terraform/user_data/worker.sh
+++ terraform/user_data/worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 MASTER_IP="${master_ip}"
 K3S_TOKEN="${token}"
-curl -sfL https://get.k3s.io | K3S_URL="https://$MASTER_IP:6443" K3S_TOKEN="$K3S_TOKEN" sh -
+curl -sfL https://get.k3s.io | K3S_URL="https://$MASTER_IP:6443" K3S_TOKEN="$K3S_TOKEN" INSTALL_K3S_EXEC="--kubelet-arg node-status-update-frequency=5s" sh -
